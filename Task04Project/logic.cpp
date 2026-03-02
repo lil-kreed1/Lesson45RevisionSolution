e8efd1dd4cb9d9be546191e9626b13935728e9df
st <st@A510PC18>
2026-03-02 18:11:56 +0300

    new

--- Task04Project/logic.cpp
+++ Task04Project/logic.cpp
@@ -8,24 +8,38 @@
 
 #include "logic.h"
 
+int find_min(int array[DEFAULT_SIZE], int size) {
+	int min = 0;
+	for (int i = 1; i < size; i++)
+	{
+		if (array[i] < array[min]) {
+			min = i;
+		}
+	}
+	return min;
+}
+
+int find_max(int array[DEFAULT_SIZE], int size) {
+	int max = 0;
+	for (int i = 1; i < size; i++)
+	{
+
+		if (array[i] > array[max]) {
+			max = i;
+		}
+	}
+	return max;
+}
+
+
 void swap_extrem_elements(int array[DEFAULT_SIZE], int size) {
-    if (size <= 0) {
-        return;
-    }
+	if (size <= 0) {
+		return;
+	}
+	int max = find_max(array, size);
+	int min = find_min(array, size);
 
-    int min = 0;
-    int max = 0;
-
-    for (int i = 1; i < size; i++) {
-        if (array[i] < array[min]) {
-            min = i;
-        }
-        if (array[i] > array[max]) {
-            max = i;
-        }
-    }
-
-    int temp = array[min];
-    array[min] = array[max];
-    array[max] = temp;
+	int temp = array[min];
+	array[min] = array[max];
+	array[max] = temp;
 }
